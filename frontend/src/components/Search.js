7c7bd513c587bc51b38059d101366a016bd77729
alvarogularte <alvaro.gularte@gmail.com>
2022-07-01 19:10:25 -0300

    new: create component Search

--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import Form from 'react-bootstrap/Form';
 
 function Search() {
   return (
-    <div>Search</div>
+    <>
+      <h3>Refine sua busca</h3>
+      <h5>Por preço</h5>
+      <Form.Check name="grouped" required label="Até R$40" type='radio' />
+      <Form.Check name="grouped" required label="R$40 - R$60" type='radio' />
+      <Form.Check name="grouped" required label="R$100 - R$200" type='radio' />
+      <Form.Check name="grouped" required label="Acima de R$500" type='radio' />
+    </>
   )
 }
 
