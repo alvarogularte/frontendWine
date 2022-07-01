905c3b43b46ec209f6210f8d12a2ea08ebfeb9ba
alvarogularte <alvaro.gularte@gmail.com>
2022-07-01 19:47:35 -0300

    new: create store and reducer

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,5 +1,8 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit';
+import selectionsReducer from './slices/selections';
 
 export default configureStore({
-  reducer: {}
+  reducer: {
+    selections: selectionsReducer
+  }
 })
